dffee4f4da66d86998b85247cdbffd2515e8a0a4
Clément Omnès <contact@clementomnes.dev>
2026-08-06 15:06:32 +0200

    feat: add cover letter

---
+++ build-cover-letter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+OUTPUT="clement-omnes-cover-letter"
+
+if [ -f .env ]; then
+    source .env
+fi
+
+PHONE="${PHONE:-}"
+PHONE_LINK="${PHONE_LINK:-}"
+LOCAL_OUTPUT_DIR="${LOCAL_OUTPUT_DIR:-$HOME/Downloads}"
+
+docker build -t sb2nov/latex .
+docker run --rm -i \
+    -v "$PWD":/data \
+    -v "${LOCAL_OUTPUT_DIR}":/output \
+    sb2nov/latex \
+    sh -c "
+        mkdir -p /tmp/build &&
+        pdflatex -jobname \"${OUTPUT}\" -output-directory /tmp/build \
+            \"\def\resumePhone{${PHONE}}\def\resumePhoneLink{${PHONE_LINK}}\input{cover-letter}\" &&
+        cp /tmp/build/${OUTPUT}.pdf /output/
+    "
